6abb81c1e9ff1c8486d5ea978a81dca708618373
Basti Tee <basti.tee@posteo.de>
2018-03-09 14:14:47 +0100

    init scripts;

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+git submodule init
+git submodule update
+git submodule add -f git@github.com:BastiTee/bottybonn-botEventCalendar.git \
+./govbot-sdk/additional_bots/botEventCalendar
+
 cd govbot-sdk
-npm install 
+npm install
 cp .env.demo .env
 cd ..
 
@@ -9,4 +14,3 @@ cd govbot-emulator
 npm install
 npm run build
 cd ..
-
